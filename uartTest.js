358d1833c34b39d7657f43fd08cc1f1dfcc667e7
Philipp Schaechtele <mail@philj.de>
2013-07-04 18:21:21 +0200

    Fixed code to work with UART on Richards Raspberry

--- uartTest.js
+++ uartTest.js
@@ -9,21 +9,21 @@ function bufferParser () {
   return function (emitter, buffer) {
     // Collect data
     data = Buffer.concat([data, buffer]);
-    
+    console.log(buffer);
     var currentChar = buffer.toString();
     // if current buffer equals an \n buffer is emitted
-    if (currentChat == delimiter) {
+//    if (currentChar == delimiter) {
       emitter.emit('data', data);
-      data = new Buffer();
-    }
+      data = Buffer(0);
+//    }
   };
 }
 
 // Init UART Serial
 var serialPort = new SerialPort("/dev/ttyAMA0", {
-  baudrate: 115200,
-  buffersize: 8,
-  parser: bufferParser
+  baudrate: 9600,
+  buffersize: 4,
+  parser: bufferParser()
 });
 
 // parses a string of bits by given format
@@ -34,14 +34,20 @@ function parseInput (input, format) {
   var output = {};
   for (var key in format) {
     if (format.hasOwnProperty(key)) {
-      // slice bits out as decribed in format .slice(start, start  + length)
-      var intAsBits = input.readUInt8(format[key][0]);
-      // parse int and write to output
-      output[key] = intAsBits;
+      var lastOffset = format[key][0] + format[key][1];
+      if (lastOffset <= input.length) {
+        // slice bits out as decribed in format .slice(start, start  + length)
+        var intAsBits = input.readUInt8(format[key][0]);
+        // parse int and write to output
+        output[key] = intAsBits;
+      } else {
+        console.log("key " + key + " offset is higher then buffer " + input.length);
+      }
     }
   } 
   return output;
 }
+
 
 // Wait on Port open
 serialPort.on("open", function () {
@@ -54,16 +60,16 @@ serialPort.on("open", function () {
     // describes name as key and start bit and length. 
     // all values are interpreted as Ints so far
     var inputFormat = {
-      val1: [0,8],
-      val2: [8, 8],
-      val3: [16, 8],
-      val4: [24, 8],
-      val5: [32,8],
-      val6: [40, 8],
-      val7: [48, 8],
-      val8: [56, 8],
-      val9: [64,8],
-      val10: [72, 8]
+      val1: [0,1],
+      val2: [1,1],
+      val3: [2,1],
+      val4: [3,1],
+//      val5: [4,1],
+//      val6: [5,1],
+//      val7: [6,1],
+//      val8: [7,1],
+//      val9: [8,1],
+//      val10: [9,1]
     };
   
     var parsedInput = parseInput(data, inputFormat);
