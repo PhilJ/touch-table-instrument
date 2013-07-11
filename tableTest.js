4f2d16faa5d60e285bda75b81fa7b3f3fbb0f8a4
Philipp Schaechtele <mail@philj.de>
2013-07-11 23:11:31 +0200

    Added color fade -broken

--- tableTest.js
+++ tableTest.js
@@ -5,6 +5,7 @@ var SerialPort = splib.SerialPort;
 var Uart = require('./UartReader.js');
 var TouchEvents = require("./TouchEvents.js").TouchEvents;
 var PixelController = require('./PixelController.js');
+var Color      = require("color");
 
 // configure table 
 var rows = 6, columns = 8;
@@ -48,14 +49,14 @@ var ledPixels = new PixelController.PixelController({
 //console.log(PixelController.createMapping(rows, columns, startX, startX, wireingDirection).unshift());
 
 // Setup pixel default value
-var input = [
+/*var input = [
   ["FF0000", "FF0000", "00FF00", "FFFF00", "FFFF00", "00FFFF", "0000FF", "0000FF"],
   ["FF0000", "FF0000", "00FF00", "FFFF00", "FFFF00", "00FFFF", "0000FF", "0000FF"],
   ["FF0000", "FF0000", "00FF00", "FFFF00", "FFFF00", "00FFFF", "0000FF", "0000FF"],
   ["FF0000", "FF0000", "00FF00", "FFFF00", "FFFF00", "00FFFF", "0000FF", "0000FF"],
   ["FF0000", "FF0000", "00FF00", "FFFF00", "FFFF00", "00FFFF", "0000FF", "0000FF"],
   ["FF0000", "FF0000", "00FF00", "FFFF00", "FFFF00", "00FFFF", "0000FF", "0000FF"],
-];
+];*/
 
 var input_white = [
   ["FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF"],
@@ -66,23 +67,51 @@ var input_white = [
   ["FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF", "FFFFFF"],
 ];
 
+var input = [
+  ["000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000"],
+  ["000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000"],
+  ["000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000"],
+  ["000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000"],
+  ["000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000"],
+  ["000000", "000000", "000000", "000000", "000000", "000000", "000000", "000000"]
+];
 
 
+var pressed = false;
 
 // on touch event
 var onTouch = function (event) {
  
   // make touched pixel white
-  input[event.newState.button.row][event.newState.button.column] = "FFFFFF";
-  ledPixels.set(input);
+  //input[event.newState.button.row][event.newState.button.column] = "FFFFFF";
+  //ledPixels.set(input);
+  
+  pressed = true;
+  lighten();
+  
+  function lighten () {
+    if (pressed == true) {
+      var oldColor = input[event.newState.button.row][event.newState.button.column];
+      
+      var newColor = Color(oldColor).lighten().hexString().slice(1,7);
+      console.log(oldColor, newColor);
+      input[event.newState.button.row][event.newState.button.column] = newColor;
+      ledPixels.set(input);
+      setTimeout(function () {
+        lighten();
+      }, 10);
+      
+    }
+  };
 //  console.log("Touch", event.newState.button);
 }
 // on release event
 var onRelease = function (event) {
   // make released pixel black
-  input[event.oldState.button.row][event.oldState.button.column] = "F0000F";
+  //input[event.oldState.button.row][event.oldState.button.column] = "F0000F";
 //  console.log("Release", event.oldState.button);
-  ledPixels.set(input);
+  //ledPixels.set(input);
+  pressed = false;
 }
   
 var onlyOnTouch = true;
