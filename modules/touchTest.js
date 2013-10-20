773b61904c14edc63c9cc5e0446ebe495dc64c3e
Philipp Schaechtele <mail@philj.de>
2013-10-20 20:59:07 +0200

    Updated MultiTouchInout , working on table now

--- modules/touchTest.js
+++ modules/touchTest.js
@@ -16,7 +16,9 @@ function touchTest (controller, tixel) {
 		e.buttonsReleased.forEach(function (position) {
 			tixel.selectAt(position);
 		});
-		tixel.setSelectedPixels('000000');
+        var fade = ['FFFFFF','EEEEEE','DDDDDD','CCCCCC','BBBBBB','AAAAAA','999999','888888',
+        '777777','666666','555555','444444','333333','222222','111111','000000']
+		tixel.setSelectedPixels(fade);
 	});
 }
 
